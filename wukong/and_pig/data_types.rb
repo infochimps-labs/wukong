4b796c38bf3b42280847f5c220c7b4e0622df2ac
Philip (flip) Kromer <flip@infochimps.org>
2009-02-01 04:56:30 -0700

    Docs

--- wukong/and_pig/data_types.rb
+++ wukong/and_pig/data_types.rb
@@ -19,12 +19,12 @@ module Wukong
   end
 end
 
-class ScalarInteger  < TypedStruct.new [
-    [:count,    Integer  ],
-  ]
-  include Wukong::AndPig::PigEmitter
-  def self.load_scalar path
-    var = super path
-    var.to_i
-  end
-end
+# class ScalarInteger  < TypedStruct.new [
+#     [:count,    Integer  ],
+#   ]
+#   include Wukong::AndPig::PigEmitter
+#   def self.load_scalar path
+#     var = super path
+#     var.to_i
+#   end
+# end
