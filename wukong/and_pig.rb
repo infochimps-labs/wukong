fa9797ec65ca9340fccfd115e7b0e9325eeb829c
Philip (flip) Kromer <flip@infochimps.org>
2009-02-05 01:39:19 -0600

    Documentation fix

--- wukong/and_pig.rb
+++ wukong/and_pig.rb
@@ -1,4 +1,3 @@
-
 require 'wukong/and_pig/pig_var'
 require 'wukong/and_pig/functions'
 require 'wukong/and_pig/operators'
@@ -11,6 +10,22 @@ require 'wukong/and_pig/symbol'
 require 'wukong/and_pig/utils'
 
 module Wukong
+  #
+  # Wukong::AndPig lets you generate and run pig[http://hadoop.apache.org/pig]
+  # code from within ruby (and interactively, from the +irb+ console).
+  #
+  # It uses the same typed structures you've defined for Wukong to create
+  # pig-types aware commands. For example, the Wukong class
+  #
+    class Customer < TypedStruct([ [:id, Integer],
+      [:name, String], [:postal_code, Integer], [:balance, Float] ])
+    end
+
+  will generate a LOAD command for pig as
+
+    Customer.pig_load('q4_reports/customers.tsv')
+    # =>
+
   module AndPig
   end
 end
