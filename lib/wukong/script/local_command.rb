2b240dba12c04c6f7bbec1a51761e7087bb40977
Yuichiro MASUI (masuidrive) <masui@masuidrive.jp>
2009-05-23 22:06:49 -0700

    add ruby interpreter path

--- lib/wukong/script/local_command.rb
+++ lib/wukong/script/local_command.rb
@@ -1,3 +1,5 @@
+require 'rbconfig'
+
 module Wukong
   module LocalCommand
 
@@ -7,7 +9,10 @@ module Wukong
     #
 
     def local_command input_path, output_path
-      %Q{ cat #{input_path} | #{map_command} | sort | #{reduce_command} > '#{output_path}'}
+      ruby_path = File.join(Config::CONFIG["bindir"],
+                            Config::CONFIG["RUBY_INSTALL_NAME"]+
+                            Config::CONFIG["EXEEXT"])
+      %Q{ cat #{input_path} | #{ruby_path} #{map_command} | sort | #{ruby_path} #{reduce_command} > '#{output_path}'}
     end
 
   end
