949586aa7fcb141cb94ba27e07f65fa5cc47426e
Yuichiro MASUI (masuidrive) <masui@masuidrive.jp>
2009-05-23 22:15:09 -0700

    revert

--- lib/wukong/script/local_command.rb
+++ lib/wukong/script/local_command.rb
@@ -1,5 +1,3 @@
-require 'rbconfig'
-
 module Wukong
   module LocalCommand
 
@@ -9,10 +7,7 @@ module Wukong
     #
 
     def local_command input_path, output_path
-      ruby_path = File.join(Config::CONFIG["bindir"],
-                            Config::CONFIG["RUBY_INSTALL_NAME"]+
-                            Config::CONFIG["EXEEXT"])
-      %Q{ cat #{input_path} | #{ruby_path} #{map_command} | sort | #{ruby_path} #{reduce_command} > '#{output_path}'}
+      %Q{ cat #{input_path} | #{map_command} | sort | #{reduce_command} > '#{output_path}'}
     end
 
   end
