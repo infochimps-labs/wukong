d98774bcbb8139bc23e078aeccec060c38d99242
Philip (flip) Kromer <flip@infochimps.org>
2009-06-22 09:44:45 -0500

    Skeleton code for: output class schema to SQL and Pig load statements; and output wukong struct as tab-separated N3 triples

---
+++ lib/wukong/schema.rb
@@ -0,0 +1,15 @@
+module Wukong
+  #
+  # Export model's structure for other data frameworks:
+  # SQL and Pig
+  #
+  module Schema
+    # def to_sql
+    # end
+    #
+    #
+    # # Export schema as Pig
+    # def to_pig
+    # end
+  end
+end
