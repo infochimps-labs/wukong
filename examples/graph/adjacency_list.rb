1b652487a429d12ab825ab5f0f8c828ea1553d5c
Philip (flip) Kromer <flip@infochimps.org>
2009-06-22 09:45:50 -0500

    Adjacency list (helpful for, among other things, a memory-efficient BFS)

---
+++ examples/graph/adjacency_list.rb
@@ -0,0 +1,74 @@
+#!/usr/bin/env ruby
+$: << ENV['WUKONG_PATH']
+require 'wukong'
+
+#
+# Given an adjacency pairs (from \t to) representation of a directed graph:
+#
+#    1     2
+#    1     7
+#    2     7
+#    2     9
+#    7     2
+#
+# It produces an adjacency list representation:
+#
+#    1  >   2   7
+#    2  >   7   9
+#    7  >   2
+#    9  >
+#
+# and
+#
+#    1  <
+#    2  <   1   7
+#    7  <   1   2
+#    9  <   2
+#
+# (each column is tab-separated in the actual output)
+#
+#
+#
+module Gen1HoodEdges
+  class Mapper < Wukong::Streamer::Base
+    def process rsrc, src, dest
+      yield [ src +'>', dest ]
+      yield [ dest+'<', src  ]
+    end
+  end
+
+  #
+  # Accumulate links onto single line.
+  #
+  # The reduce key is the target node and direction; we just stream through all
+  # pairs for each target node and output its neighbor nodes on the same line.
+  #
+  # To control memory usage, we will print directly to the output (and not run
+  # through the Emitter)
+  #
+  class Reducer < Wukong::Streamer::AccumulatingReducer
+    # clear the list of incoming paths
+    def start! target, *args
+      @dir = target.slice!(-1)   # remove last character: it's the direction
+      print target + "\t" + @dir # start line with target and list type
+    end
+    def accumulate target, neighbor
+      print "\t" + neighbor      # append neighbor to output, same line
+    end
+    def finalize
+      puts ''                    # start new line
+    end
+  end
+
+  class Script < Wukong::Script
+    def default_options
+      super.merge :sort_fields => 1, :partition_fields => 1
+    end
+  end
+end
+
+# Execute the script
+Gen1HoodEdges::Script.new(
+  Gen1HoodEdges::Mapper,
+  Gen1HoodEdges::Reducer
+  ).run
