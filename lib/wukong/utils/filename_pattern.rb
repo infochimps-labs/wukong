2fc760372382ea007bfdbae5e05226659a034112
Philip (flip) Kromer <flip@infochimps.org>
2009-07-17 12:04:59 -0500

    Command to (tab-separate + space align) a tab-separated table of values. Intelligent but not very efficient

--- lib/wukong/utils/filename_pattern.rb
+++ lib/wukong/utils/filename_pattern.rb
@@ -1,38 +1,41 @@
 module Wukong
   module Utils
     class FilenamePattern
-      attr_accessor :pattern
-      def initialize pattern
+      attr_accessor :pattern, :token_val_defaults
+      def initialize pattern, token_val_defaults={}
         self.pattern = pattern
+        self.token_val_defaults = token_val_defaults
       end
 
       #
       # walk through pattern, replacing tokens (eg :time or :pid) with the
       # corresponding value.
       #
-      def make
-        @timestamp = Time.now.utc.to_flat
-        val = pattern.gsub(/:\w+/){|token| token_val(token)  }
-        @timestamp = nil
+      def make token_vals={}
+        token_vals = token_vals.reverse_merge token_val_defaults
+        token_vals[:timestamp] ||= Time.now.utc.to_flat
+        val = pattern.gsub(/:(\w+)/){ replace($1, token_vals)  }
         val
       end
 
       #
       # substitute for token
       #
-      def token_val token
-        p token
+      def replace token, token_vals
+        token = token.to_sym
         case token
-        when ':pid'        then pid
-        when ':hostname'   then hostname
-        when ':datetime'   then @timestamp
-        when ':date'       then @timestamp[ 0..7]
-        when ':time'       then @timestamp[ 8..13]
-        when ':hour'       then @timestamp[ 8..9]
-        when ':min'        then @timestamp[10..11]
-        when ':sec'        then @timestamp[12..13]
+        when :pid           then pid
+        when :hostname      then hostname
+        when :handle        then token_vals[:handle]
+        when :handle_prefix then token_vals[:handle][0..5]
+        when :datetime      then token_vals[:timestamp]
+        when :date          then token_vals[:timestamp][ 0..7]
+        when :time          then token_vals[:timestamp][ 8..13]
+        when :hour          then token_vals[:timestamp][ 8..9]
+        when :min           then token_vals[:timestamp][10..11]
+        when :sec           then token_vals[:timestamp][12..13]
         else
-          raise "Don't know how to encode token #{token}"
+          token_vals[token] or raise "Don't know how to encode token #{token} #{token_vals[token]}"
         end
       end
 
@@ -42,7 +45,7 @@ module Wukong
       end
       # Memoized: the Process ID for this invocation.
       def pid
-        @pid ||= Process.pid
+        @pid      ||= Process.pid
       end
 
       # Characters deemed safe in a filename;
