d17ea1fd575e28a6d2f1e0f7f6b8371dd42fbaa0
Philip (flip) Kromer <flip@infochimps.org>
2009-07-17 12:03:51 -0500

    Using autoload

--- lib/wukong/streamer.rb
+++ lib/wukong/streamer.rb
@@ -1,9 +1,13 @@
-require 'wukong/streamer/base'
-require 'wukong/streamer/line_streamer'
-require 'wukong/streamer/struct_streamer'
-#
-require 'wukong/streamer/filter'
-#
-require 'wukong/streamer/accumulating_reducer'
-require 'wukong/streamer/list_reducer'
-require 'wukong/streamer/uniq_by_last_reducer'
+module Wukong
+  module Streamer
+    autoload :Base,                 'wukong/streamer/base'
+    autoload :LineStreamer,         'wukong/streamer/line_streamer'
+    autoload :StructStreamer,       'wukong/streamer/struct_streamer'
+    #
+    autoload :Filter,               'wukong/streamer/filter'
+    #
+    autoload :AccumulatingReducer,  'wukong/streamer/accumulating_reducer'
+    autoload :ListReducer,          'wukong/streamer/list_reducer'
+    autoload :UniqByLastReducer,    'wukong/streamer/uniq_by_last_reducer'
+  end
+end
