f887ea64dbec7e4e5b0f48365dd753f3ab89af55
Philip (flip) Kromer <flip@infochimps.org>
2009-07-28 18:41:47 -0500

    BadRecord is like BadLieutant with a G Rating. Lets you serialize errors in-stream, by just rejecting the record that caused the error

--- lib/wukong.rb
+++ lib/wukong.rb
@@ -2,6 +2,7 @@ require 'wukong/boot'
 require 'wukong/extensions'
 require 'wukong/datatypes'
 require 'wukong/logger'
+require 'wukong/bad_record'
 autoload :TypedStruct, 'wukong/typed_struct'
 module Wukong
   autoload :Dfs,         'wukong/dfs'
