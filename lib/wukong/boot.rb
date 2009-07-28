37ed82584483f62acc54dd548061e20ecf2ffd0e
Philip (flip) Kromer <flip@infochimps.org>
2009-07-28 10:19:58 -0500

    Alright the whole loading a config file is too much once ruby and ruby19 and jruby start ganging up with different load paths and interpretations of __FILE__. So now you get an environment varialble or ~/.wukong.rb

--- lib/wukong/boot.rb
+++ lib/wukong/boot.rb
@@ -1,18 +1,46 @@
 module Wukong
-  CONFIG_FILE_LOCATION=File.dirname(__FILE__)+'/../../config'
-  ::Wukong::CONFIG      = { }
+
+  # ---------------------------------------------------------------------------
+  #
+  # Default options for Wukong
+  #   http://github.com/infochimps/wukong
+  #
+  # If you set an environment variable WUKONG_CONFIG, *or* if the file
+  # $HOME/.wukong.rb exists, that file will be +require+'d as well.
+  #
+  # Important values to set:
+  #
+  # * Wukong::CONFIG[:hadoop_home] --
+  #   Path to root of hadoop install. If your hadoop runner is
+  #     /usr/local/share/hadoop/bin/hadoop
+  #   then your hadoop_home is
+  #     /usr/local/share/hadoop.
+  #   You can also set a
+  #
+  # * Wukong::CONFIG[:default_run_mode] -- Whether to run using hadoop (and
+  #   thus, requiring a working hadoop install), or to run in local mode
+  #   (script --map | sort | script --reduce)
+  #
+  CONFIG = {
+    # Run as local or as hadoop?
+    :default_run_mode => 'hadoop',
+
+    # The command to run when a nil mapper or reducer is given.
+    :default_mapper   => '/bin/cat',
+    :default_reducer  => '/bin/cat',
+
+    # Anything in HADOOP_OPTIONS_MAP (see lib/wukong/script/hadoop_command.rb)
+    :runner_defaults => {
+    },
+  }
+
   def self.config_options
-
-    # load main options
-    require CONFIG_FILE_LOCATION+'/wukong'
-
     # # override with site-specific options
-    site_config_filename = CONFIG_FILE_LOCATION+'/wukong-site'
-    require site_config_filename if File.exists?(site_config_filename+'.rb')
+    site_config_filename = ENV['WUKONG_CONFIG'] || (ENV['HOME'].to_s+'/.wukong.rb')
+    require site_config_filename.gsub(/\.rb$/,'') if File.exists?(site_config_filename)
 
     # try to guess a hadoop_home if none given
-    Wukong::CONFIG[:hadoop_home] ||= ENV['HADOOP_HOME']
-
+    Wukong::CONFIG[:hadoop_home] ||= ENV['HADOOP_HOME'] || '/usr/lib/hadoop'
   end
   self.config_options
 end
