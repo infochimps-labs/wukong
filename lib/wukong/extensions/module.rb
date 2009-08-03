5fb81b170b1a88577d1610d09d2fda9c671cabf1
Philip (flip) Kromer <flip@infochimps.org>
2009-08-02 19:53:41 -0500

    Removed old implementation of find_const

--- lib/wukong/extensions/module.rb
+++ lib/wukong/extensions/module.rb
@@ -1,28 +1,2 @@
 require 'extlib/object'
 require 'extlib/module'
-
-# Module.module_eval do
-#   # Tries to find a constant with the name specified in the argument string:
-#   #
-#   #   "Module".constantize     # => Module
-#   #   "Test::Unit".constantize # => Test::Unit
-#   #
-#   # The name is assumed to be the one of a top-level constant, no matter whether
-#   # it starts with "::" or not. No lexical context is taken into account:
-#   #
-#   #   C = 'outside'
-#   #   module M
-#   #     C = 'inside'
-#   #     C               # => 'inside'
-#   #     "C".constantize # => 'outside', same as ::C
-#   #   end
-#   #
-#   # NameError is raised when the name is not in CamelCase or the constant is
-#   # unknown.
-#   def constantize const_name
-#     unless /\A(?:::)?([A-Z]\w*(?:::[A-Z]\w*)*)\z/ =~ const_name
-#       raise NameError, "#{self.inspect} is not a valid constant name!"
-#     end
-#     self.module_eval("#{$1}", __FILE__, __LINE__)
-#   end
-# end
