caf282cb6b191d37ffe39072d057c4c299e70294
Philip (flip) Kromer <flip@infochimps.org>
2009-10-24 22:53:26 -0500

    Oops misspelled input_file

--- lib/wukong/streamer/base.rb
+++ lib/wukong/streamer/base.rb
@@ -17,7 +17,7 @@ module Wukong
       # Pass each record to +#process+
       #
       def stream
-        Log.info("Streaming on:\t%s" % [Script.inputfile])
+        Log.info("Streaming on:\t%s" % [Script.input_file])
         before_stream
         $stdin.each do |line|
           record = recordize(line.chomp)
