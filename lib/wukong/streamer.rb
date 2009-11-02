6532040416d589fd46039f01a8791119cf93d388
Philip (flip) Kromer <flip@infochimps.org>
2009-11-02 01:19:56 -0600

    Added record streamer -- should derive from this, not abstract base I think

--- lib/wukong/streamer.rb
+++ lib/wukong/streamer.rb
@@ -2,6 +2,7 @@ module Wukong
   module Streamer
     autoload :Base,                 'wukong/streamer/base'
     autoload :LineStreamer,         'wukong/streamer/line_streamer'
+    autoload :RecordStreamer,       'wukong/streamer/record_streamer'
     autoload :StructStreamer,       'wukong/streamer/struct_streamer'
     #
     autoload :Filter,               'wukong/streamer/filter'
