0a1f4ee1a39bd90f4d3da42275109cf910635005
Philip (flip) Kromer <flip@infochimps.org>
2010-06-20 08:41:09 +0000

    a first draft of a periodic logger / runner

---
+++ lib/wukong/periodic_monitor.rb
@@ -0,0 +1,57 @@
+Settings.define :log_interval, :default => 1000, :type => Integer, :description => 'How many iterations between log statements'
+
+#
+# Periodic logger
+#
+#
+# This is very much a work in progress
+#
+class PeriodicLogger
+  attr_reader   :iter, :start_time, :options
+  attr_accessor :interval
+
+  def initialize extra_options={}
+    @options      = {}
+    @options.deep_merge!( extra_options || {} )
+    @iter         = 0
+    @start_time   = now
+    @interval     = (options[:log_interval] || Settings[:log_interval]).to_i
+    @interval = 1000 unless @interval >= 1
+  end
+
+  def periodically *args, &block
+    incr!
+    if ready?
+      if block
+        block.call(iter, *args)
+      else
+        $stderr.puts progress(*args)
+      end
+    end
+  end
+
+  def incr!
+    @iter += 1
+  end
+
+  def ready?
+    iter % @interval == 0
+  end
+
+  def progress *stuff
+    [
+      "%15d" % iter,
+      "%7.2f"% elapsed_time, "sec",
+      "%7.2f"%(iter.to_f / elapsed_time), "/sec",
+      now.to_flat,
+      *stuff
+    ].join("\t")
+  end
+
+  def elapsed_time
+    now - start_time
+  end
+  def now
+    Time.now.utc
+  end
+end
