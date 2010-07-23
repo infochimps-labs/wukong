46ffeb1486be9078f0674ad1902334f2e591e2cd
Philip (flip) Kromer <flip@infochimps.org>
2010-07-23 14:26:56 -0500

    example that will break until chunked_flat_file_store works

---
+++ examples/store/chunked_store_example.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'wukong'
+# require 'wukong/store/chunked_flat_file_store'
+
+require 'configliere'
+Configliere.use :commandline, :define, :config_file
+Settings.read('foo.yaml')
+
+# store = ChunkedFlatFileStore.new(Settings)
+
+100.times do |iter|
+  # store.save   [iter, Time.now.to_flat].join("\t")
+  $stdout.puts [iter, Time.now.to_flat].join("\t")
+  sleep 2
+end
+
+
