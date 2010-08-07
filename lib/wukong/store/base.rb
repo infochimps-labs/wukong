1db08183f3ba4b030b7c09944535326639738c0b
howech <chris@howeville.com>
2010-08-07 07:20:02 -0500

    Added files to support write only chunked flat file stores.

--- lib/wukong/store/base.rb
+++ lib/wukong/store/base.rb
@@ -1,18 +1,16 @@
-module Monkeyshines
+module Wukong
   module Store
     class Base
-      attr_accessor :options
-      def initialize _options={}
-        self.options = _options
-        Log.info "Creating #{self.class}"
+      def initialize options={}
+        Log.info "Creating #{self.class} with #{options.inspect}"
       end
 
-      #
+      #Iterate through each object casting it as a new object of klass.
       def each_as klass, &block
         self.each do |*args|
           begin
             item = klass.new *args[1..-1]
-          rescue Exception => e
+          rescue StandardError => e
             Log.info [args, e.to_s, self].join("\t")
             raise e
           end
