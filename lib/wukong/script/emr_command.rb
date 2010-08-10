2e220c9d11ed0518edcbc1fe49f6ae2cace96851
Philip (flip) Kromer <flip@infochimps.org>
2010-08-10 09:09:03 -0500

    Reworked Script to have a much more coherent and compelling plot

--- lib/wukong/script/emr_command.rb
+++ lib/wukong/script/emr_command.rb
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 module Wukong
   module HadoopCommand
 
--- lib/wukong/script/local_command.rb
+++ lib/wukong/script/local_command.rb
@@ -8,14 +8,20 @@ module Wukong
 
     # program, including arg, to sort input between mapper and reducer in local
     # mode. You could override to for example run 'sort -n' (numeric sort).
-    def sort_command
+    def local_mode_sort_commandline
       'sort'
     end
 
-    def local_command input_path, output_path
+    #
+    # Commandline string to execute the job in local mode
+    #
+    # With an input path of '-', just uses $stdin
+    # With an output path of '-', just uses $stdout
+    #
+    def local_commandline input_path, output_path
       cmd_input_str  = (input_path  == '-') ? "" : "cat '#{input_path}' | "
       cmd_output_str = (output_path == '-') ? "" : "> '#{output_path}'"
-      %Q{ #{cmd_input_str} #{map_command} | #{sort_command} | #{reduce_command} #{cmd_output_str} }
+      %Q{ #{cmd_input_str} #{mapper_commandline} | #{local_mode_sort_commandline} | #{reducer_commandline} #{cmd_output_str} }
     end
 
   end
