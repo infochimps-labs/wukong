a0f1912899746adc4b0288dfb2280f326ecbddde
Philip (flip) Kromer <flip@infochimps.org>
2010-08-10 06:12:19 -0500

    avro notes

---
+++ lib/wukong/script/emr_command.rb
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+module Wukong
+  module HadoopCommand
+
+    # ===========================================================================
+    #
+    # EMR Options
+    #
+
+
+
+  end
+end
