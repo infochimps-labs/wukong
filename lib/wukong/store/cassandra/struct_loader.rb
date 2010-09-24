96f01518e5c2dfbfeba28aff567abf5f488ae403
Philip (flip) Kromer <flip@infochimps.org>
2010-09-24 04:45:19 -0500

    Cassandra object streamer + tw feed = yay

--- lib/wukong/store/cassandra/struct_loader.rb
+++ lib/wukong/store/cassandra/struct_loader.rb
@@ -1,16 +1,19 @@
+module Wukong::Store::Cassandra
 
-class StructLoader < Wukong::Streamer::StructStreamer
-  def initialize *args
-    super(*args)
-    @log = PeriodicMonitor.new
+  class StructLoader < Wukong::Streamer::StructStreamer
+    def initialize *args
+      super(*args)
+      @log = PeriodicMonitor.new
+    end
+
+    #
+    # Blindly expects objects streaming by to have a "streaming_save" method
+    #
+    def process object, *_
+      object.save
+      # object.streaming_save
+      @log.periodically(object.to_flat)
+    end
   end
 
-  #
-  # Blindly expects objects streaming by to have a "streaming_save" method
-  #
-  def process object, *_
-    object.save
-    # object.streaming_save
-    @log.periodically(object.to_flat)
-  end
 end
