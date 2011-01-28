493363484d332a054e6e5bf6466c3b0ca80e75e1
Philip (flip) Kromer <flip@infochimps.org>
2011-01-28 14:06:55 +0000

    Clearing out coprolites

--- lib/wukong/monitor.rb
+++
@@ -1,7 +0,0 @@
-module Monkeyshines
-  module Monitor
-    autoload :PeriodicMonitor, 'monkeyshines/monitor/periodic_monitor'
-    autoload :PeriodicLogger,  'monkeyshines/monitor/periodic_logger'
-  end
-end
-
