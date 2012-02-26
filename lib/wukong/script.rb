ad99c5016e8e6aa4ed4d97db941353fb45d92778
Philip (flip) Kromer <flip@infochimps.org>
2012-02-26 14:39:04 -0600

    moved script stuff into wukong/script

---
+++ lib/wukong/script.rb
@@ -0,0 +1,36 @@
+require 'wukong'
+
+#
+# Runner settings
+#
+
+Settings.define :mode, :type => Symbol, :default => :mapper
+
+module Wukong
+  # adds ability to execute
+  extend Wukong::Mixin::FromFile
+
+  #
+  # sources a script file,
+  #
+  class Script
+    attr_reader :settings     # configliere hash of options
+    attr_reader :script_file  # File to execute
+
+    def initialize(script_file, settings)
+      @script_file = script_file
+      @settings = settings
+    end
+
+    # Execute the script file in the context of the Wukong module
+    def run
+      Log.info( "Running #{script_file} with settings #{settings}")
+      script_file = self.script_file
+      mode        = settings.mode
+      Wukong.module_eval do
+        from_file(script_file)
+        flow(mode).run
+      end
+    end
+  end
+end
