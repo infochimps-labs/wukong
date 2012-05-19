2b7a7fa884827915220fd284ddcce8e3e051f6e1
Philip (flip) Kromer <flip@infochimps.org>
2012-05-19 15:18:50 -0400

    added an example dataflow, and a data file to test with

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ require 'gorillib/pathname'
 Pathname.register_path(:wukong_root, File.expand_path('..', File.dirname(__FILE__)))
 Pathname.register_path(:examples,    :wukong_root, 'examples')
 Pathname.register_path(:tmp,         :wukong_root, 'tmp')
+Pathname.register_path(:data,        :wukong_root, 'data')
+Pathname.path_to(:tmp).mkpath
 
 $LOAD_PATH.unshift(Pathname.path_to('lib'))
 $LOAD_PATH.unshift(Pathname.path_to('spec', 'support'))
