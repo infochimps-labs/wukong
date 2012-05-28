e71c30208704bf8aa397cfedf766d3284ad27e72
Travis Dempsey <dempsey.travis@gmail.com>
2012-05-28 15:22:49 -0500

    cleaned up paths; use simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,15 @@
-require 'bundler/setup' ; Bundler.require(:default, :test)
+require 'bundler' ; Bundler.require(:development)
+SimpleCov.start do
+  add_filter '/gorillib/'
+end
+require 'wukong'
 require 'gorillib/pathname'
-SimpleCov.start
 
 Pathname.register_path(:wukong_root, File.expand_path('..', File.dirname(__FILE__)))
 Pathname.register_path(:examples,    :wukong_root, 'examples')
 Pathname.register_path(:tmp,         :wukong_root, 'tmp')
 Pathname.register_path(:data,        :wukong_root, 'data')
 Pathname.path_to(:tmp).mkpath
-
-$LOAD_PATH.unshift(Pathname.path_to('lib'))
-$LOAD_PATH.unshift(Pathname.path_to('spec', 'support'))
 
 Dir[ Pathname.path_to('spec', 'support', '*.rb') ].each{|f| require f }
 
