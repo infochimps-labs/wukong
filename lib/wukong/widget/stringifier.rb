4baa41c4f9323d5b9ea380c734e3274ad916e2b3
Dhruv Bansal <dhruv@ph.utexas.edu>
2012-06-06 15:24:53 -0500

    modifications to allow stdin/stdout to transmit event bodies along with headers, and json used as a wire format can encode/decode models on the fly

--- lib/wukong/widget/stringifier.rb
+++ lib/wukong/widget/stringifier.rb
@@ -12,8 +12,20 @@ module Wukong
     end
 
     class FromJson < Stringifier
+      # FIXME some of this belongs in gorillib factories...
       def process(record)
-        emit MultiJson.load(record)
+        obj = MultiJson.load(record)
+        if obj.respond_to?(:has_key?) && obj.has_key?("_metadata")
+          metadata_hash = obj.delete("_metadata")
+          obj.define_singleton_method(:_metadata) do
+            metadata_hash
+          end
+        end
+        if obj.respond_to?(:has_key?) && obj.has_key?("_type")
+          klass = Gorillib::Factory(obj.delete("_type"))
+          obj = klass.receive(obj)
+        end
+        emit obj
       end
       register_processor
     end
