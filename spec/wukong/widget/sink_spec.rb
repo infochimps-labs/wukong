f59fbd20a7303b126a91a7033ea7821afb0dd624
Philip (flip) Kromer <flip@infochimps.org>
2012-06-11 22:33:23 -0500

    more processors, and mixins for sources (that may or may not stick around)

--- spec/wukong/widget/sink_spec.rb
+++ spec/wukong/widget/sink_spec.rb
@@ -3,14 +3,17 @@ require 'spec_helper'
 describe :sinks, :helpers => true do
 
   describe Wukong::Sink::Stdout do
-    it 'dumps records to $stdout'
-    # $stdout.should_receive(:puts).with(mock_record)
-    # subject.call(mock_record)
+    it 'dumps records to $stdout' do
+      $stdout.should_receive(:puts).with(mock_record)
+      subject.process(mock_record)
+    end
   end
 
   describe Wukong::Sink::Stderr do
-    it 'dumps records to $stderr'
-    # $stderr.should_receive(:puts).with(mock_record)
-    # subject.call(mock_record)
+    it 'dumps records to $stderr' do
+      $stderr.should_receive(:puts).with(mock_record)
+      subject.process(mock_record)
+    end
+
   end
 end
--- spec/wukong/widget/source_spec.rb
+++ spec/wukong/widget/source_spec.rb
@@ -1,8 +1,32 @@
 require 'spec_helper'
+require 'wukong/widget/gibberish'
 
 describe :sources, :helpers => true do
+  describe Wukong::Source::Integers do
+    subject{ described_class.receive(:size => 10) }
+    before{ subject.setup }
 
-  describe Wukong::Source::Iter do
+    it 'generates integers up to the given limit' do
+      subject.to_enum.to_a.should == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    end
+    it 'generates nothing if the initial range is void' do
+      subject.size = 0
+      subject.to_enum.to_a.should == []
+    end
+    it 'generates one thing if the min and max are equal' do
+      subject.size = 1
+      subject.to_enum.to_a.should == [0]
+    end
+  end
+
+  describe Wukong::Widget::Gibberish do
+    subject{ described_class.receive(:size => 3) }
+    before{ subject.setup }
+
+    it 'generates integers up to the given limit' do
+      subject.rng = Random.new(8675309)
+      subject.to_enum.to_a.should == ["loaiaeiaeo neidgfo heeume sptfmeec naet sttptlm waaaioh detov elrrltv nii ulcsnn", "set ensr poeleaa seqi tmnreoee boooral oczncgp deaia rykcoao leo rim mmibpbfii", "artrru sto quuu doo peoehrile nto esl tia gaili tuiooey lkedotp sts kaiy weeeia", "crhi tyiiae mieubmbooa teeae roi ednz taieh zaloy syhe ret kuoa deeeo xittipl mo"]
+    end
 
   end
 end
