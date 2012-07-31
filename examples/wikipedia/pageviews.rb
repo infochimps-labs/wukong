04e17b86bff84692dad4ea44a641cd632170d28b
Dieterich Lawson <dieterich.lawson@gmail.com>
2012-07-31 12:16:11 -0500

    First pass at pageview script

--- examples/wikipedia/pageviews.rb
+++ examples/wikipedia/pageviews.rb
@@ -1,13 +1,14 @@
 #!/usr/bin/env ruby
 
 require 'wukong'
+require 'uri'
+require 'pathname'
 module PageviewsToTSV
   class Mapper < Wukong::Streamer::LineStreamer
 
 # change spaces to tabs
 # un-urlencode names
 # change namespace name to number
-=begin
   NAMESPACES = {
     "Main" => 0,"" => 0,"Talk" => 1,
     "User" => 2,"User_Talk" => 3,
@@ -20,10 +21,40 @@ module PageviewsToTSV
     "Portal" => 100,"Portal_talk" => 101,
     "Book" => 108, "Book_talk" => 109,
   }
-=end 
-    # grab file name
+  
+  # the filename strings are formatted as
+  # pagecounts-YYYYMMDD-HH0000.gz
+    def time_from_filename(filename)
+      parts = filename.split('-')
+      year = parts[1][0..3].to_i
+      month = parts[1][4..5].to_i
+      day = parts[1][6..7].to_i
+      hour = parts[2][0..1].to_i
+      return Time.new(year,month,day,hour)
+    end
+
+  # grab file name
     def process line
-      yield ENV.pretty_inspect
+      fields = line.split(' ')[1..-1]
+      out_fields = []
+      # add the namespace
+      out_fields << NAMESPACES[fields[0].split(':')[0]]
+      # add the title
+      out_fields << URI.unescape(fields[0][fields[0].index(':')+1..-1])
+      # add number of visitors in the hour
+      out_fields << fields[2]
+      # grab date info from filename
+      file = Pathname.new(ENV['map_input_file']).basename
+      time = time_from_filename(file)
+      out_fields << time.year
+      out_fields << time.month
+      out_fields << time.day
+      out_fields << time.hour
+      # seconds since unix epoch
+      out_fields << time.to_i
+      # day of the week
+      out_fields << time.wday
+      yield out_fields
     end
   end
 end
