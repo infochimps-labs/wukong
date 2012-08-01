41bb10502f7c7a43c774d5e7bd7d7c5a57bf7c7b
Dieterich Lawson <dieterich.lawson@gmail.com>
2012-08-01 16:09:55 -0500

    Added UTF-8 handling to to_tsv.rb

--- examples/wikipedia/to_tsv.rb
+++ examples/wikipedia/to_tsv.rb
@@ -20,25 +20,37 @@ module ToTSV
 
     @@line_num = 0
 
+    def guard_encoding line, &blk
+      blk.call(line)
+    rescue StandardError => err
+      $invalid_lines +=1
+      repaired_line = []
+      line.each_char do |char|
+        if char.valid_encoding?
+          repaired_line << char
+        else
+          $invalid_chars +=1
+          repaired_line << "?"
+        end
+      end
+      blk.call(repaired_line.join)
+    end
+
     def process line
-      @@line_num+=1
-      match_num=0
-      begin
-        return unless line =~ /INSERT INTO/
-        line.scan(RECORD_RE).each do |fields|
+      guard_encoding(line) do |safe_line|
+        @@line_num+=1
+        match_num=0
+        return unless safe_line =~ /INSERT INTO/
+        safe_line.scan(RECORD_RE).each do |fields|
           match_num+=1
           STRING_FIELDS.each do |index|
             orig = fields[index].dup
             fields[index] = fields[index][1..-2]
             fields[index].gsub!(/\\(['"\\])/,'\1')
           end
-          line = fields.join("\t")
+          safe_line = fields.join("\t")
           yield fields
         end
-      rescue StandardError => ex
-        $stderr.puts "*** EXCEPTION on line: #{@@line_num} match num: #{match_num} ***"
-        $stderr.puts ex.message
-        $stderr.puts ex.backtrace
       end
     end
   end
