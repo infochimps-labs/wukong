f543116c9c4ae5f4aa833433f2eeb21b0b50887a
Dieterich Lawson <dieterich.lawson@gmail.com>
2012-08-01 16:54:12 -0500

    Removed erroneous variable in char_filter

--- examples/wikipedia/char_filter.rb
+++ examples/wikipedia/char_filter.rb
@@ -5,13 +5,11 @@ def guard_encoding line, &blk
   if line.valid_encoding?
     blk.call(line)
   else 
-    $invalid_lines +=1
     repaired_line = []
     line.each_char do |char|
       if char.valid_encoding?
         repaired_line << char
       else
-        $invalid_chars +=1
         repaired_line << "�"
       end
     end
