14f296cbccbece85df6a3433adc444a8754f54b2
Philip (flip) Kromer <flip@infochimps.org>
2012-08-12 08:49:53 -0500

    modernizing specs and spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,14 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-
-require 'wukong'
-require 'spec'
-require 'spec/autorun'
-
-Spec::Runner.configure do |config|
-
+if ENV['WUKONG_COV']
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/gorillib/'
+    add_filter '/away/'
+    add_group  'Hanuman', '/hanuman/'
+  end
 end
 
+require 'wukong'
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+end
--- spec/wukong/script_spec.rb
+++ spec/wukong/script_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "Wukong::Script" do
   before do
-    ARGV.replace []
+    ARGV.replace ['in', 'out']
     @script = Wukong::Script.new 'mapper', 'reducer'
   end
 
