f374780b98d544b8ba4c11968e2906c67ee2362f
Dieterich Lawson <dieterich.lawson@gmail.com>
2012-08-28 14:26:02 -0500

    Added fancy version of page metadata extractor

--- examples/munging/wikipedia/page_metadata/extract_page_metadata.rb
+++ examples/munging/wikipedia/page_metadata/extract_page_metadata.rb
@@ -1,27 +1,14 @@
 #!/usr/bin/env ruby
 
 require 'wukong'
+require 'wukong/streamer/sql_streamer'
 
-load '/home/dlaw/dev/wukong/examples/wikipedia/munging_utils.rb'
-
-module PagesToTSV
-  class Mapper < Wukong::Streamer::LineStreamer
-
-    COLUMNS=  [:int, :int, :string, :string, :int, 
-               :int, :int, :float, :string, :int, :int]
-  
-    def initialize
-      @sql_parser = MungingUtils::SQLParser.new(COLUMNS)
-    end
-
-    def process(line, &blk)
-      @sql_parser.parse(line,&blk)
-    end
-  end
+module PageMetdataExctractor
+  class Mapper < Wukong::Streamer::SQLStreamer
+    #TODO: Add encoding guard
+    columns [:int, :int, :string, :string, :int, 
+             :int, :int, :float, :string, :int, :int]
+   end
 end
 
-# go to town
-Wukong::Script.new(
-  PagesToTSV::Mapper,
-  nil
-).run
+Wukong::Script.new(PageMetadataExtractor::Mapper, nil).run
