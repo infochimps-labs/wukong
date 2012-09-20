599f8eda380436aa323f10377c81eef5d01358f3
Travis Dempsey <dempsey.travis@gmail.com>
2012-09-20 11:50:48 -0500

    cleanup of examples ; formatting

--- examples/dataflow/parse_apache_logs.rb
+++ examples/dataflow/parse_apache_logs.rb
@@ -2,9 +2,8 @@ require File.expand_path('../examples_helper', File.dirname(__FILE__))
 require Pathname.path_to(:examples, 'dataflow/apache_log_line')
 
 ExampleUniverse.dataflow(:parse_apache_logs) do
-  doc <<-DOC
-    Parses an apache log line into a structured model, emits it as JSON
-  DOC
+
+  doc 'Parses an apache log line into a structured model, emits it as JSON'
 
   input  :default, file_source(Pathname.path_to(:data, 'log/sample_apache_log.log'))
   output :dump,    stdout 
