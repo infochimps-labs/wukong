90e574be10e5959b8dfa43679f16921f362141a2
Travis Dempsey <dempsey.travis@gmail.com>
2012-10-29 17:45:27 -0500

    spec helper is being cleaned

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
-require 'bundler' ; Bundler.require(:default, :development)
+require 'bundler' ; Bundler.setup(:default, :development)
 
 if ENV['WUKONG_COV']
   require 'simplecov'
+
   SimpleCov.start do
     add_filter '/gorillib/'
     add_filter '/away/'
@@ -10,24 +11,26 @@ if ENV['WUKONG_COV']
 end
 
 require 'wukong'
-require 'gorillib/model'
-require 'gorillib/pathname'
-require 'gorillib/type/extended'
-require 'wukong/model/faker'
+require_relative './support/shared_examples_for_builders'
+require_relative './support/shared_examples_for_shortcuts'
 
-Pathname.register_path(:wukong_root, File.expand_path('..', File.dirname(__FILE__)))
-Pathname.register_path(:examples,    :wukong_root, 'examples')
-Pathname.register_path(:tmp,         :wukong_root, 'tmp')
-Pathname.register_path(:data,        :wukong_root, 'data')
-Pathname.path_to(:tmp).mkpath
+# require 'gorillib/pathname'
+# require 'gorillib/type/extended'
+# require 'wukong/model/faker'
 
-Dir[ Pathname.path_to('spec', 'support', '*.rb') ].each{|f| require f }
+# Pathname.register_path(:wukong_root, File.expand_path('..', File.dirname(__FILE__)))
+# Pathname.register_path(:examples,    :wukong_root, 'examples')
+# Pathname.register_path(:tmp,         :wukong_root, 'tmp')
+# Pathname.register_path(:data,        :wukong_root, 'data')
+# Pathname.path_to(:tmp).mkpath
 
-result   = `dot -V 2>&1` rescue nil
-GRAPHVIZ = ($?.exitstatus == 0) && (result =~ /dot - graphviz version/)
-puts 'Some specs require graphviz to run -- brew/apt install graphviz, it is pretty awesome' unless GRAPHVIZ
+# Dir[ Pathname.path_to('spec', 'support', '*.rb') ].each{|f| require f }
 
-RSpec.configure do |config|
-  include WukongTestHelpers
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-end
+# result   = `dot -V 2>&1` rescue nil
+# GRAPHVIZ = ($?.exitstatus == 0) && (result =~ /dot - graphviz version/)
+# puts 'Some specs require graphviz to run -- brew/apt install graphviz, it is pretty awesome' unless GRAPHVIZ
+
+# RSpec.configure do |config|
+#   include WukongTestHelpers
+#   config.treat_symbols_as_metadata_keys_with_true_values = true
+# end
