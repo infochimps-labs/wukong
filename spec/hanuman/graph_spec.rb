b47721a656f96c3650edd728f17569332d31ea8e
Travis Dempsey <dempsey.travis@gmail.com>
2012-10-29 17:47:24 -0500

    refactored spec to match new hanuman interface ; passing

--- spec/hanuman/graph_spec.rb
+++ spec/hanuman/graph_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
-require 'hanuman'
 
-describe Hanuman::Graph, :helpers => true do
+describe Hanuman::Graph, :hanuman => true do
 
   # it 'makes a tree' do
   #   example_graph.tree.should == {
@@ -14,4 +13,30 @@ describe Hanuman::Graph, :helpers => true do
   #     }
   # end
 
+end
+
+describe Hanuman::GraphBuilder, :hanuman => true do
+
+  it_behaves_like 'a Stage::Builder'
+  
+  its(:namespace){ should be(Hanuman::Graph) }
+  
+  context '#define' do
+    let(:block_arg){ ->(){ def say() "I'm the Juggernaut!" ; end } }
+    
+    it 'evalutes a supplied block itself' do
+      subject.label = :juggernaut
+      subject.should_receive(:instance_eval).with(&block_arg)
+      subject.define(&block_arg)
+    end
+  end
+  
+  context '#build' do
+  end
+  
+  context '#serialize' do
+    it 'serializes into a Hash with stages and links' do
+      subject.serialize.should include(:stages, :links)
+    end  
+  end
 end
