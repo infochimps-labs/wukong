24f4e39f320ade40aa55d25867cf9f6a1e8c1ed3
Travis Dempsey <dempsey.travis@gmail.com>
2012-11-02 18:05:54 -0500

    added notification methods back into processor

--- lib/wukong/processor.rb
+++ lib/wukong/processor.rb
@@ -7,8 +7,9 @@ module Wukong
 
   class Processor < Hanuman::Stage
 
-    field :action, Whatever
-    field :log,    Whatever, :default => Log
+    field :action,   Whatever
+    field :log,      Whatever, :default => Log
+    field :notifier, Vayacondios::NotifierFactory, :default => Vayacondios.default_notifier
 
     # This is a placeholder method intended to be overridden
     def perform_action(*args) ; end 
@@ -16,6 +17,17 @@ module Wukong
     # The action attribute is turned into the perform action method
     def receive_action(action)
       self.define_singleton_method(:perform_action, &action)
+    end
+
+    # Valid notifier types are currently :http or :log
+    # This processor's log is passed to vayacondios
+    def receive_notifier(type)
+      @notifier = Vayacondios::NotifierFactory.receive(type: type, log: log)
+    end
+
+    # Send information to Vayacondios; data goes in, the right thing happens
+    def notify(topic, cargo)
+      notifier.notify(topic, cargo)
     end
 
     # This method is called after the processor class has been instantiated
