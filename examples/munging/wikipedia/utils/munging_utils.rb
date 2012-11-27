9c1493404090ab22ca2f51fae3b1bd3fa06e11f9
Philip (flip) Kromer <flip@infochimps.org>
2012-11-27 06:12:45 +0000

    wikipedia article extractor now does the XML parsing for you -- it's so variable and sloppy it shouldn't be allowed downstream.

--- examples/munging/wikipedia/utils/munging_utils.rb
+++ examples/munging/wikipedia/utils/munging_utils.rb
@@ -1,5 +1,11 @@
 # encoding:UTF-8
 module MungingUtils
+  extend self # you can call MungingUtils.foo, or include it and call on self.
+
+  # all non-keyboard characters (that is, characters outside the 0x20 to 0x127 range)
+  NON_PLAIN_ASCII_RE = /[^\x20-\x7e]/m
+  # characters below 0x20
+  CONTROL_CHARS_RE   = /[\x00-\x19]/m
 
   def time_columns_from_time(time)
     columns = []
@@ -15,10 +21,36 @@ module MungingUtils
     Log.warn [desc, record_info].join("\t")
     nil
   end
+
+  # Modifies the text in place, replacing all newlines, tabs, and other control
+  # characters with a space (those < ascii 0x20, but not including 0xff). This
+  # uses a whitelist
+  #
+  # Only use this if funny characters aren't suppose to be in there in the first
+  # place; there are safe, easy ways to properly encode, eg `MultiJson.encode()`
+  #
+  def scrub_control_chars(text)
+    text.gsub!(CONTROL_CHARS_RE, ' ')
+    text
+  end
+
+  # Modifies the text in place, replacing all non-keyboard characters (newline,
+  # tab, anything not between ascii 0x20 and 0x7e) with their XML entity encoding
+  def flatten_xml_text(text)
+    text.gsub!(NON_PLAIN_ASCII_RE){|char| "&##{char.ord};" };
+    text
+  end
+
+end
+
+Time.class_eval do
+  def to_flat
+    utc.strftime("%Y%m%d%H%M%SZ")
+  end
 end
 
 MatchData.class_eval do
   def as_hash
-    Hash[ names.map{|name| [name, self[name]] } ]
+    Hash[ names.map{|name| [name.to_sym, self[name]] } ]
   end
 end
