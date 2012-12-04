c5e4cc3c8cf3227b8a777a0c0663c3f2eacfa09f
Travis Dempsey <dempsey.travis@gmail.com>
2012-12-04 16:56:04 -0600

    added recordizer; cleaned up logic around serialization

--- spec/wukong/widget/serializers_spec.rb
+++ spec/wukong/widget/serializers_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 shared_context 'serializers', serializer: true do
   let(:bad_record){ nil }
-  let(:serializer){ create_processor(self.class.top_level_description) }  
+  let(:serializer){ create_processor(self.class.top_level_description, on_error: :skip) }  
 
   it_behaves_like 'a serializer'
 end
@@ -31,6 +31,16 @@ describe :to_json, serializer: true do
       serializer.given(valid_record).should emit("{\n  \"hi\": \"there\"\n}")
     end  
   end
+
+  context 'given a model' do
+    let(:json_record) {  '{"foo":"bar"}'                      }
+    let(:valid_model) { double('model', to_json: json_record) }
+    
+    it 'defers to the model to serialize' do
+      valid_model.should_receive(:to_json).and_return(json_record)
+      serializer.given(valid_model).should emit(json_record)
+    end
+  end
 end
 
 describe :to_tsv, serializer: true do
@@ -38,6 +48,16 @@ describe :to_tsv, serializer: true do
   
   it 'serializes valid records' do
     serializer.given(valid_record).should emit("foo\t2\ta")
+  end
+
+  context 'given a model' do
+    let(:tsv_record)  { "foo\tbar\tbaz"                     }
+    let(:valid_model) { double('model', to_tsv: tsv_record) }
+    
+    it 'defers to the model to serialize' do
+      valid_model.should_receive(:to_tsv).and_return(tsv_record)
+      serializer.given(valid_model).should emit(tsv_record)
+    end
   end
 end
 
@@ -48,6 +68,16 @@ describe :from_json, serializer: true do
   it 'deserializes valid records' do
     serializer.given(valid_record).should emit({'hi' => 'there'})
   end  
+
+  context 'given a model' do
+    let(:wire_format) { { foo: 'bar' }                          }
+    let(:valid_model) { double('model', from_json: wire_format) }
+    
+    it 'defers to the model to serialize' do
+      valid_model.should_receive(:from_json).and_return(wire_format)
+      serializer.given(valid_model).should emit(wire_format)
+    end
+  end
 end
 
 describe :from_tsv, serializer: true do
@@ -56,5 +86,29 @@ describe :from_tsv, serializer: true do
   it 'deserializes valid records' do
     serializer.given(valid_record).should emit(['foo', '2', 'a' ])
   end
+
+  context 'given a model' do
+    let(:wire_format) { { foo: 'bar' }                         }
+    let(:valid_model) { double('model', from_tsv: wire_format) }
+    
+    it 'defers to the model to serialize' do
+      valid_model.should_receive(:from_tsv).and_return(wire_format)
+      serializer.given(valid_model).should emit(wire_format)
+    end
+  end
 end
 
+describe :inspect do
+  it_behaves_like 'a processor'
+end
+
+describe :recordize, serializer: true do
+  let(:model_instance) { double('model')                                                   }
+  let(:model_klass)    { double('model_def', receive: model_instance)                      }
+  let(:serializer)     { create_processor(:recordize, model: model_klass, on_error: :skip) }
+  let(:valid_record)   { { foo: 'bar' }                                                    }
+
+  it 'recordizes valid records' do
+    serializer.given(valid_record).should emit(model_instance)
+  end
+end
