99dfcd002396dfc6503d4d3bea9118db72bbc8fc
Travis Dempsey <dempsey.travis@gmail.com>
2012-12-04 15:41:23 -0600

    added shared examples and context for serializers; refactor

--- spec/wukong/widget/serializers_spec.rb
+++ spec/wukong/widget/serializers_spec.rb
@@ -1,92 +1,60 @@
 require 'spec_helper'
 
-describe "Serializing" do
+shared_context 'serializers', serializer: true do
+  let(:bad_record){ nil }
+  let(:serializer){ create_processor(self.class.top_level_description) }  
 
-  context :to_json do
-    
-    let(:emittable)     { {"hi" => "there"} }
-    let(:not_emittable) { {"n" => Float::INFINITY} }
+  it_behaves_like 'a serializer'
+end
 
-    it_behaves_like 'a processor', :named => :to_json
-    
-    it "should handle valid records" do
-      processor.given(emittable).should emit_json(emittable)
-    end
+shared_examples_for 'a serializer' do
+  it_behaves_like 'a processor'
 
-    it "should skip bad records" do
-      processor.given(not_emittable).should emit(0).records
-    end
-    
-  end
-
-  context :to_tsv do
-    let(:emittable)     { ["foo", 2, :a] }
-    let(:not_emittable) { nil            }
-
-    it_behaves_like 'a processor', :named => :to_tsv
-    
-    it "should handle valid records" do
-      processor.given(emittable).should emit_tsv(emittable.map(&:to_s))
-    end
-    
-    it "should skip bad records" do
-      processor.given(not_emittable).should emit(0).records
-    end
+  it 'handles errors on bad records' do
+    serializer.should_receive(:handle_error).with(bad_record, a_kind_of(Exception)).and_return(nil)
+    serializer.given(bad_record).should emit(0).records
   end
 end
 
-describe "Deserializing" do
+describe :to_json, serializer: true do
+  let(:valid_record) { { hi: 'there' }        }
+  let(:bad_record)   { { no: Float::INFINITY} }
 
-  context :from_json do
-    let(:parseable)     { '{"hi": "there"}' }
-    let(:not_parseable) { '{"832323:'       }
-
-    it_behaves_like 'a processor', :named => :from_json
-
-    it "should handle valid records" do
-      processor.given(parseable).should emit({'hi' => 'there'})
-    end
-    
-    it "should skip bad records" do
-      processor.given(not_parseable).should emit(0).records
-    end
+  it 'serializes valid records' do
+    serializer.given(valid_record).should emit('{"hi":"there"}')
   end
+
+  context 'pretty' do
+    let(:serializer){ create_processor(:to_json, pretty: true) }
+    
+    it 'prettifies valid records' do
+      serializer.given(valid_record).should emit("{\n  \"hi\": \"there\"\n}")
+    end  
+  end
+end
+
+describe :to_tsv, serializer: true do
+  let(:valid_record) { ["foo", 2, :a] }
   
-  context :from_tsv do
-
-    let(:parseable)     { "foo\t2\ta"    }
-    let(:not_parseable) { nil            }
-
-    it_behaves_like 'a processor', :named => :from_tsv
-    
-    it "should handle valid records" do
-      processor.given(parseable).should emit(parseable.split("\t"))
-    end
-    
-    it "should skip bad records" do
-      processor.given(not_parseable).should emit(0).records
-    end
+  it 'serializes valid records' do
+    serializer.given(valid_record).should emit("foo\t2\ta")
   end
 end
 
-describe "Pretty printing" do
+describe :from_json, serializer: true do
+  let(:valid_record) { '{"hi": "there"}' }
+  let(:bad_record)   { '{"832323:'       }
+  
+  it 'deserializes valid records' do
+    serializer.given(valid_record).should emit({'hi' => 'there'})
+  end  
+end
 
-  context "JSON" do
-    let(:parseable)     { '{"hi": "there"}' }
-    let(:not_parseable) { '{"832323:'       }
-
-    it_behaves_like 'a processor', :named => :pretty
-
-    it "should prettify parseable records" do
-      processor(:pretty).given(parseable).should emit_json({'hi' => 'there'})
-    end
-
-    it "should pass on non parseable records" do
-      processor(:pretty).given(not_parseable).should emit(not_parseable)
-    end
-  end
-
-  it "should pass on everything else" do
-    processor(:pretty).given('foobar').should emit('foobar')
+describe :from_tsv, serializer: true do
+  let(:valid_record) { "foo\t2\ta" }
+  
+  it 'deserializes valid records' do
+    serializer.given(valid_record).should emit(['foo', '2', 'a' ])
   end
 end
+
