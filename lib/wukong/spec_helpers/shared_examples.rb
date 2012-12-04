1130518d51464e2791f6ad363daf49eb3507b058
Travis Dempsey <dempsey.travis@gmail.com>
2012-12-04 13:34:30 -0600

    use implicit subject

--- lib/wukong/spec_helpers/shared_examples.rb
+++ lib/wukong/spec_helpers/shared_examples.rb
@@ -4,11 +4,12 @@ shared_examples_for 'a processor' do |options={}|
     it "is registered with the name '#{name}'" do
       Wukong.registry.retrieve(name.to_sym).should_not be_nil
     end
-    it{ create_processor(name).should respond_to(:setup)    }
-    it{ create_processor(name).should respond_to(:process)  }
-    it{ create_processor(name).should respond_to(:finalize) }
-    it{ create_processor(name).should respond_to(:stop)     }
-    it{ create_processor(name).should respond_to(:notify)   }
+    subject{ create_processor(name)  }
+    it{ should respond_to(:setup)    }
+    it{ should respond_to(:process)  }
+    it{ should respond_to(:finalize) }
+    it{ should respond_to(:stop)     }
+    it{ should respond_to(:notify)   }
   else
     warn "Must supply a name for a processor you want to test"
   end
