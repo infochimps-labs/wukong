27b5c3ae0af5ece527e5c6d3dedcbb0054692564
Dhruv Bansal <dhruv@ph.utexas.edu>
2013-01-04 14:51:47 -0600

    added documentation helpers for generating complete YARD documentation from DSL methods

---
+++ lib/wukong/doc_helpers.rb
@@ -0,0 +1,14 @@
+require 'yard'
+require 'wukong'
+require_relative('doc_helpers/processor_handler')
+require_relative('doc_helpers/dataflow_handler')
+require_relative('doc_helpers/field_handler')
+
+module Wukong
+
+  # This module defines several YARD handlers for processing inline
+  # documentation written around Wukong processors and dataflows and
+  # Gorillib fields when using the DSL.
+  module DocHelpers
+  end
+end
