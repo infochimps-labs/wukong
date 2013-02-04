98acd5deb0acf2a550867eeda0101d9bf7da5301
Dhruv Bansal <dhruv@ph.utexas.edu>
2013-02-03 21:10:19 -0600

    made group by type widgets emit an array, not a string

--- spec/wukong/widget/reducers/group_spec.rb
+++ spec/wukong/widget/reducers/group_spec.rb
@@ -5,16 +5,16 @@ describe "Reducers" do
     include_context "reducers"
     it_behaves_like 'a processor', :named => :group
 
-    let(:grouped_strings) { [['apple', '2'], ['banana', '1'], ['cookie', '1']]             }
-    let(:grouped_nums)    { [['', '2'], ['1', '1'], ['5', '1'], ['10', '1'], ['100', '1']] }
+    let(:grouped_strings) { [['apple', 2], ['banana', 1], ['cookie', 1]]  }
+    let(:grouped_nums)    { [[nil, 2], [1, 1], [5, 1], [10, 1], [100, 1]] }
     it "will group single values" do
-      processor(:group).given(*strings.sort).should emit_tsv(*grouped_strings)
+      processor(:group).given(*strings.sort).should emit(*grouped_strings)
     end
     it "can group from within a JSON hash" do
-      proc = processor(:group, by: 'data.n').given(*json_sorted_n).should emit_tsv(*grouped_nums)
+      proc = processor(:group, by: 'data.n').given(*json_sorted_n).should emit(*grouped_nums)
     end
     it "can group from within a TSV row" do
-      proc = processor(:group, by: '3').given(*tsv_sorted).should emit_tsv(*grouped_nums)
+      proc = processor(:group, by: '3').given(*tsv_sorted).should emit(*grouped_nums)
     end
   end
 end
