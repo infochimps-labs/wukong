d3d43eb5727be54b6af722c2ecff59bd1c41851d
Dieterich Lawson <dieterich.lawson@gmail.com>
2013-02-17 20:58:10 -0600

    Added default options value for hadoop runner

--- lib/wukong/script/hadoop_runner.rb
+++ lib/wukong/script/hadoop_runner.rb
@@ -4,7 +4,7 @@ module Wukong
   module Script
     module HadoopRunner
       extend self
-      def run args, opts
+      def run args, opts={}
         cmd = ['hadoop'] + args
         Gorillib::System::Runner.run(cmd,opts)
       end
